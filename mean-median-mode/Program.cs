0d8846d211f10fbc3b9140eece5157302d377f87
sahil-naik-kilowott <92841842+sahil-naik-kilowott@users.noreply.github.com>
2021-11-30 18:20:42 +0530

    Add files via upload

---
+++ mean-median-mode/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] data = { 40, 38, 37, 36, 34, 33, 32, 31, 30, 30, 30, 29, 26, 26, 19 };
+            int n = data.Length;
+
+
+            Stats.sorting(data, n);
+            Console.WriteLine("Mean : "+ Stats.findmean(data, n));
+            Console.WriteLine("Median : " + Stats.findmedian(data, n));
+            Console.WriteLine("Mode : " + Stats.findmode(data, n));
+
+            
+        }
+    }
+}
---
+++ mean-median-mode/Stats.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    internal class Stats
+    {
+        //function to sort the array
+        public static void sorting(int[] data, int n)
+        {
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = i + 1; j < data.Length; j++)
+                {
+                    if (data[i] > data[j])
+                    {
+                        int temp = data[i];
+                        data[i] = data[j];
+                        data[j] = temp;
+                    }
+
+                }
+
+            }
+        }
+       
+
+        //function to find mean
+        public static double findmean(int[] data, int n)
+        {
+            int sum = 0;
+            
+            for (int i = 0; i < n; i++)
+            {
+                sum += data[i];
+
+            }
+
+            return (double)sum / (double)n;
+
+        }
+
+
+        //function to find median
+        public static double findmedian(int[] data, int n)
+        {
+            if(n%2!=0)
+            {
+                return (double)data[n/2];
+
+            }
+            else
+            {
+                return (double)(data[(n-1)/2] + data[n/2]) / 2.0;
+            }
+
+        }
+
+
+        ////function to find mode
+        public static double findmode(int[] data, int n)
+        {
+            int maximum_num = -1;
+            int max_repeat = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int count = 0;
+
+                for (int j = 0; j < data.Length; j++)
+                {
+
+                    if(data[i]==data[j])
+                    {
+                        count++;
+                    }
+                }
+
+                if(count>max_repeat)
+                {
+                    maximum_num = data[i];
+                    max_repeat = count;
+                }
+
+            }
+
+            return maximum_num;
+
+        }
+
+
+
+
+    }
+
+}
